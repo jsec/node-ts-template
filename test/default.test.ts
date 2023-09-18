a946ebe5520d7dc6dfe83c79e3cfc17ec7f7b1e1
Jarrod Seccombe <Jarrod.Seccombe@icloud.com>
2023-09-18 12:36:55 -0400

    build(test): replace ava with mocha/chai combination

---
+++ test/default.test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+import { add } from '../src';
+
+describe('add', () => {
+  it('adds two numbers', () => {
+    const result = add(1, 2);
+    expect(result).to.equal(3);
+  });
+});
