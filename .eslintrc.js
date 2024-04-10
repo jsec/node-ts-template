f34663e062e1c1d71aca87b39426e9292e77f23b
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-04-10 19:46:54 -0400

    style(eslint): update @jarsec/eslint-config to 5.1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['@jarsec/eslint-config']
+  extends: [ '@jarsec/eslint-config' ]
 };
--- test/default.test.ts
+++ test/default.test.ts
@@ -1,6 +1,6 @@
-import {expect, test} from 'vitest';
+import { expect, test } from 'vitest';
 
-import {add} from '../src';
+import { add } from '../src';
 
 test('adds two numbers', () => {
   const result = add(1, 2);
